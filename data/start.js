3575878862c4befb1c12b0c30492cbadf767ba66
BrunoTiptc <brunimptc2009@hotmail.com>
2026-04-29 20:39:46 -0300

    Iniciando declarando variaveis no basico, aorendido que aqui o jeito correto não é emitir com var, (perigozo) o certo aqui no js é let e const

---
+++ data/start.js
@@ -0,0 +1,14 @@
+//Declarando variaveis
+
+const nome = "Bruno";
+let idade = 30;
+
+//Imprimindo as variaveis
+
+console.log("Meu nome é " + nome + " e minha idade é " + idade + " \n" );
+
+//Ou de forma mais legivel usando template literals
+
+console.log(`Meu nome é ${nome} e minha idade é ${idade}`); 
+
+
