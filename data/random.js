a5cbcd88e0d06784ad1d80bf718e25730f170b31
BrunoTiptc <brunimptc2009@hotmail.com>
2026-05-04 20:00:53 -0300

    Explorando a função randon e math, que no caso dessa linguagem nao precisa importa biblioteca.

---
+++ data/random.js
@@ -0,0 +1,3 @@
+let aleatorio = Math.floor(Math.random() * 51) + 50;
+
+console.log(" O numero aleatorio é: " + aleatorio);
---
+++ data/variaveis.js
@@ -0,0 +1,21 @@
+let idade = 30; // Inteiro (integer)
+let valor = 100.50; // Decimal (float)
+
+console.log(10 % 2); // Resto da divisão (modulo)
+console.log(10 ** 2); // Potenciação (exponenciação)    -- ao quadrado
+
+let erro = "texto" * 2;
+console.log(erro); // NaN (Not a Number) - resultado de uma operação inválida com números
+
+let infinito = 10 / 0;
+console.log(infinito); // Infinity - resultado de uma divisão por zero
+
+console.log(Math.round(4.7)); // Arredonda para o inteiro mais próximo (5)
+console.log(Math.floor(4.7)); // Arredonda para baixo (4)
+console.log(Math.ceil(4.2)); // Arredonda para cima (5) 
+
+console.log(Math.random()); // Gera um número aleatório entre 0 (inclusive) e 1 (exclusivo)
+console.log(Math.random() * 100); // Gera um número aleatório entre 0 e 100
+console.log(Math.floor(Math.random() * 100)); // Gera um número inteiro aleatório entre 0 e 99  
+console.log(Math.PI); // Valor de pi (3.141592653589793)
+
