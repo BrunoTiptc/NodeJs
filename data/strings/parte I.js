f1a2071231a03d1477b930430297831ae868446b
BrunoTiptc <brunimptc2009@hotmail.com>
2026-05-05 20:05:25 -0300

    Aprofundando meus conhecimentos em strings no JS

---
+++ data/strings/parte I.js
@@ -0,0 +1,10 @@
+function analiseString(str) {
+    console.log(" \n A string é: " + str);
+    console.log(" \n O tamanho da string é: " + str.length);
+    console.log(" \n A primeira letra da string é: " + str[0]);
+    console.log(" \n A última letra da string é: " + str[str.length - 1]);
+     // - 1 É o truque para pegaer o ultimo caracter 
+
+}
+
+analiseString("Bruno");
---
+++ data/strings/strings.js
@@ -0,0 +1,8 @@
+let name = "Bruno";
+let lastname = "César";
+
+console.log("O meu nome completo é: " + name + " " + lastname);
+
+console.log(`O meu nome completo é: ${name} ${lastname} \n`);
+
+console.log(" O tamanho da string é " + name.length); // Vai mostrar o tamanho da string
