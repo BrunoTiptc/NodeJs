97faee10a9afb83c6a90d594840feef62bf459a9
BrunoTiptc <brunimptc2009@hotmail.com>
2026-05-25 22:23:47 -0300

    ecerxicio diario

---
+++ data/variaveis/verificação.js
@@ -0,0 +1,15 @@
+function facaIsto() {
+    console.log(" O input está indefinido ou seja vazio");
+}
+
+function facaAquilo() {
+    console.log(" O input tem um valor definido");
+}
+
+let input;
+
+if (input === undefined) {
+    facaIsto();
+} else {
+    façaAquilo();
+}
